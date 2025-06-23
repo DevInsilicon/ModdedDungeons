f574672f5153517d909f2e58181a35bf0eb77feb
Jack <89612893+DevInsilicon@users.noreply.github.com>
2025-06-23 16:58:56 -0500

    Update ItemManager.java

--- src/main/java/dev/insilicon/moddedDungeons/ItemManagement/ItemManager.java
+++ src/main/java/dev/insilicon/moddedDungeons/ItemManagement/ItemManager.java
@@ -2,14 +2,17 @@ package dev.insilicon.moddedDungeons.ItemManagement;
 
 import dev.insilicon.moddedDungeons.ItemManagement.Items.BaseItem;
 import dev.insilicon.moddedDungeons.ItemManagement.Items.Basic.Item.BasicWand;
+import dev.insilicon.moddedDungeons.ModdedDungeons;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 public class ItemManager implements Listener {
     public static NamespacedKey itemKey = new NamespacedKey("modded_dungeons", "item_key");
@@ -32,8 +35,23 @@ public class ItemManager implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (event.getItem() == null) return;
-        if (!event.getItem().getPersistentDataContainer().has(itemKey)) return;
 
-        String itemName = event.getItem().getPersistentDataContainer().get(itemKey, PersistentDataType.STRING);
+
+
+    }
+
+    public static BaseItem getItemType(ItemStack item) {
+        if (!item.getPersistentDataContainer().has(itemKey, PersistentDataType.STRING)) return null;
+
+        String itemName = item.getPersistentDataContainer().get(itemKey, PersistentDataType.STRING);
+        if (itemName == null) return null;
+
+        for (BaseItem item2 : ItemTypes) {
+            if (item2.name.equals(itemName)) {
+                return item2;
+            }
+        }
+        ModdedDungeons.instance.getLogger().log(Level.WARNING,"Item not found?? " + itemName);
+        return null;
     }
 }
